7e59f086bf276c663634d11767d9a0d4d1b233af
DualScope <DualScope@users.noreply.github.com>
2025-06-23 10:47:12 +0100

    Updates

--- my-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the application without crashing', () => {
-  render(<App />);
-  const element = screen.getByText(/Los Angeles Based Director/i);
-  expect(element).toBeInTheDocument();
-});
